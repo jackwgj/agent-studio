1feb22ac4658b52fadcf1f56a1da519272722204
blank1058 <liukang7464@163.com>
2026-06-24 18:19:53 +0800

    fix:去除容器内部的健康检查脚本
    Co-authored-by: liukang<liukang7464@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1307 merge studio-2.0 into studio-2.0
    
    fix:去除容器内部的健康检查脚本
    
    Created-by: blank1058
    Commit-by: liukang
    Merged-by: yangyangliu
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind <label>
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [ ] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [ ] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [ ] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [ ] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [ ] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    
    See merge request: openJiuwen/agent-studio!1307

--- docker/studio-runtime/script/start.sh
+++ docker/studio-runtime/script/start.sh
@@ -18,25 +18,10 @@ function after_start(){
   nohup bash -c "source ${SHELL_DIR}/cron_job.sh && start_cron_job" &
 }
 
-function start_watchdog() {
-    if [[ "${HEALTH_CHECK:-true}" != "true" ]]; then
-        return 0
-    fi
-    sleep 10
-    # 检查任务是否已存在，避免重复添加
-    if ps -ef | grep watchdog| grep -v grep ; then
-      echo "watchdog is already start."
-      return 0
-    fi
-    nohup bash ${SHELL_DIR}/watchdog.sh &
-}
-
 function main() {
   chmod 750 ${SERVICE_HOME}
 
   bash ${SHELL_DIR}/start_server.sh
-
-  start_watchdog
 
   after_start
 }
