d8b95b606200af8b6b33048f5d60687c49f5d517
blank1058 <liukang7464@163.com>
2026-07-13 11:00:58 +0800

    fix:删除数据源管理相关的代码
    Co-authored-by: blank1058<liukang7464@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1530 merge studio-2.0-security into studio-2.0
    
    fix:删除数据源管理相关的代码
    
    Created-by: blank1058
    Commit-by: blank1058
    Merged-by: yangyangliu
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind <label>
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [ ] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [ ] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [ ] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [ ] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [ ] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    
    See merge request: openJiuwen/agent-studio!1530

---
+++ backend/studio-manager-api/src/main/java/com/openjiuwen/studio/agent/manager/dto/BatchDeleteRsp.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ */
+
+package com.openjiuwen.studio.agent.manager.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 批量删除响应体
+ */
+@ApiModel(description = "批量删除响应体")
+
+@Validated
+
+public class BatchDeleteRsp implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("count")
+    @NotNull
+    private Long count = null;
+
+    @JsonProperty("ids")
+    @Valid
+    @Size(min = 1, max = 100)
+    private List<@Pattern(regexp = "^[a-zA-Z0-9_-]+$") @Length(min = 1, max = 64) String> ids = null;
+
+    public Long getCount() {
+        return count;
+    }
+
+    public BatchDeleteRsp setCount(Long count) {
+        this.count = count;
+        return this;
+    }
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public BatchDeleteRsp setIds(List<String> ids) {
+        this.ids = ids;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class BatchDeleteRsp {\n");
+
+        sb.append("    count: ").append(toIndentedString(count)).append("\n");
+        sb.append("    ids: ").append(toIndentedString(ids)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BatchDeleteRsp batchDeleteRsp = (BatchDeleteRsp) o;
+        return Objects.equals(this.count, batchDeleteRsp.count) && Objects.equals(this.ids,
+            batchDeleteRsp.ids);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count, ids);
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
