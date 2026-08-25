0fc43f8de78f9f162f67746b2b989c869c75094b
chen <1796321429@qq.com>
2026-08-25 11:26:45 +0800

    优化接口代码

--- backend/studio-manager-service/src/main/java/com/openjiuwen/studio/agent/manager/mapper/SIESkillMapper.java
+++ backend/studio-manager-service/src/main/java/com/openjiuwen/studio/agent/manager/mapper/SIESkillMapper.java
@@ -9,25 +9,7 @@ import com.openjiuwen.studio.agent.manager.entity.SkillEntity;
 import java.util.List;
 
 public interface SIESkillMapper {
-    /**
-     * 插入 Skill 基本信息
-     * @return 新建的版本 id
-     */
-    int insert(SkillEntity skill);
-
-    /**
-     * 更新 Skill 基本信息
-     * @return 更新的行数
-     */
-    int update(String skillId, SkillEntity skill);
-
-    /**
-     * 删除 Skill 基本信息
-     * @return 删除的行数
-     */
-    int delete(String skillId);
-
-    /**
+        /**
      * 模糊搜索
      */
     List<SkillDetails> search(SkillEntity skill, int offset, int limit, String priorityStatus, String tagId, Integer publishedAsset);
@@ -36,21 +18,8 @@ public interface SIESkillMapper {
      * 获取符合要求的 Skill 的数量
      */
     int countSelectedSkills(SkillEntity skill);
-
     /**
      * 查询 Skill 详情
      */
     SkillDetails searchBySkillId(String skillId);
-
-    /**
-     * 根据 skillIds 批量查询 Skills
-     */
-    List<SkillDetails> searchBySkillIds(List<String> skillIds);
-
-    /**
-     * 根据 Skill Id 和 domain Id 查询
-     */
-    SkillDetails searchBySkillIdAndDomainId(String skillId, String domainId);
-
-
 }
